7fbaee1f096f586b270aa7649ee518d69c51d75b
T_cat9000_2 <terpstragraham@gmail.com>
2025-09-23 19:57:08 -0700

    Add files via upload

---
+++ api.py
@@ -0,0 +1,11 @@
+from flask import Flask, request, jsonify
+import os
+wburl = os.getenv("WEBHOOK")
+app = Flask(__name__)
+@app.route("/api", methods=["POST"])
+def message():
+    data = request.get_json()
+    message = data.get("message")
+    __import__("requests").post(wburl, json={"content": message})
+if __name__ == "__main__":
+    app.run(debug=True)
