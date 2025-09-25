b6f892b4ca4488bf68a991547b3ea95fd65ba395
T_cat9000_2 <terpstragraham@gmail.com>
2025-09-25 16:05:44 -0700

    trying this

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, send_file
 import os
 wburl = os.getenv("WEBHOOK")
 app = Flask(__name__)
@@ -7,5 +7,9 @@ def message():
     data = request.get_json()
     message = data.get("message")
     __import__("requests").post(wburl, json={"content": message})
+@app.route("/")
+def index():
+    return send_file("index.html")
 if __name__ == "__main__":
     app.run(debug=True)
+
